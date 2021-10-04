3bb08092fd449b78f5714ec1dd925815ab28c23b
mustafaalmhboubabdullrhman <91890928+mustafaalmhboubabdullrhman@users.noreply.github.com>
2021-10-04 13:58:09 +0100

    Add files via upload

---
+++ operators.kt
@@ -0,0 +1,17 @@
+
+fun main(args: Array<String>){
+      var number1 = 5 
+      var number2 = 3 
+      var sum = number1 + number2
+      var sub = number1 - number2 
+      var mult = number1 * number2
+      var divide = number1 / number2
+      var mod = number1 % number2
+
+    println("$sum")
+    println("$sub")
+    println("$mult")
+    println("$divide")
+    println("$mod")
+   
+    }
